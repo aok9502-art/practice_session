d997e2b87b381292c2857b0e6a987f938db31b63
Yun <yun@yuns-lab.myfiosgateway.com>
2026-01-19 12:21:08 -0500

    Completed stubs and main function

--- polygon.py
+++ polygon.py
@@ -6,3 +6,12 @@ def get_triangle_base(radius, angle):
     return 0.0
 def get_polygon_area(n, radius):
     return 0.0
+
+def main():
+    get_interior_angle()
+    get_triangle_height()
+    get_triangle_base()
+    get_polygon_area()
+    
+if __name__ == "main":
+    main()
