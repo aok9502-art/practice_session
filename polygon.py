a67bc9c3a5b0ad62a8926713c9a3ed99aeebf946
Yun <yun@yuns-lab.myfiosgateway.com>
2026-01-19 12:49:18 -0500

    Now is a GPA calculator, the intial push the code had not been saved.

--- polygon.py
+++ polygon.py
@@ -1,18 +1,6 @@
-def get_interior_angle(n):
-    return 360/n
-def get_triangle_height(radius, angle):
+def grade_to_value(letter):
     return 0.0
-def get_triangle_base(radius, angle):
-    return 0.0
-def get_polygon_area(n, radius):
-    return 0.0
-
-def main():
-    pass
-##    get_interior_angle()
-##    get_triangle_height()
-##    get_triangle_base()
-##    get_polygon_area()
-##    
-if __name__ == "__main__":
-    main()
+def class_quality_points(credits, letter):
+    credits * letter
+def calculate_gpa(total_points, total_credits):
+    
