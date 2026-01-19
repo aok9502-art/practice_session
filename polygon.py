5e398ca86a5c48dd433384f6485a312c03792201
Yun <yun@yuns-lab.myfiosgateway.com>
2026-01-19 12:15:18 -0500

    File added

---
+++ polygon.py
@@ -0,0 +1,8 @@
+def get_interior_angle(n):
+    return 0.0
+def get_triangle_height(radius, angle):
+    return 0.0
+def get_triangle_base(radius, angle):
+    return 0.0
+def get_polygon_area(n, radius):
+    return 0.0
